bca90fafce0511955e78090b3b7fb9d4b9bc431b
abstract-animation-9914 <vm930@hotmail.com>
2019-01-16 04:27:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,18 +47,9 @@ def monopoly_with_third_tier
       :reading_railroad=>
       {
       },
-      :pennsylvania_railroad=>
-      {
-       
-      },
-      :b_and_o_railroad=>
-      {
-        
-      },
-      :shortline=>
-      {
-        
-      }
+      :pennsylvania_railroad,
+      :b_and_o_railroad,
+      :shortline
     }
   }
 }
